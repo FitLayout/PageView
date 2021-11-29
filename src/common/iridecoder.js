985d63b06ab391200370b48171fe3eac9a2f3119
Radek Burget <burgetr@fit.vutbr.cz>
2021-11-29 13:09:42 +0100

    Added OWL prefix to IRI decoder

--- src/common/iridecoder.js
+++ src/common/iridecoder.js
@@ -4,6 +4,7 @@ export default class IriDecoder {
 		rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
 		rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
 		xsd: 'http://www.w3.org/2001/XMLSchema#',
+		owl: 'http://www.w3.org/2002/07/owl#',
 		b: 'http://fitlayout.github.io/ontology/render.owl#',
 		a: 'http://fitlayout.github.io/ontology/segmentation.owl#',
 		fl: 'http://fitlayout.github.io/ontology/fitlayout.owl#',
