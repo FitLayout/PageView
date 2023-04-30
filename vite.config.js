d71299d30a6a02edfe4e084f66bbb785c9f9ddaf
Radek Burget <burgetr@fit.vutbr.cz>
2023-04-30 10:22:10 +0200

    Vite config fixes

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      nodePolyfills({
+        // To exclude specific polyfills, add them to this list.
+        exclude: [
+          'fs', // Excludes the polyfill for `fs` and `node:fs`.
+        ],
+        // Whether to polyfill `node:` protocol imports.
+        protocolImports: true,
+      }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    port: 3000
+  },
+  base: '/browser/'
 })
